51309ff5b66303edf50024b734e0c5eda33f09ca
Schatzj <jakota.schatz@gmail.com>
2025-03-11 21:51:39 -0600

    Correcting the sort order.

--- src/main/java/views/FileExplorerView.java
+++ src/main/java/views/FileExplorerView.java
@@ -5,7 +5,9 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
@@ -74,20 +76,38 @@ public class FileExplorerView {
 	}
 
 	private FileExplorerNode addNodes(FileExplorerNode parentDirectory, File dir, String targetPath) {
-		File[] files = dir.listFiles();
+		File[] files = dir.listFiles();		
 		List<File> fileList = Arrays.asList(files);
-		fileList.sort(new Comparator<File>() {
+		fileList.sort(new Comparator<File>() {	
 
 			@Override
 			public int compare(File o1, File o2) {
+				Map<String, Integer> correctMonthOrder = new HashMap<>();
+				correctMonthOrder.put("January", 1);
+				correctMonthOrder.put("February", 2);
+				correctMonthOrder.put("March", 3);
+				correctMonthOrder.put("April", 4);
+				correctMonthOrder.put("May", 5);
+				correctMonthOrder.put("June", 6);
+				correctMonthOrder.put("July", 7);
+				correctMonthOrder.put("August", 8);
+				correctMonthOrder.put("September", 9);
+				correctMonthOrder.put("October", 10);
+				correctMonthOrder.put("November", 11);
+				correctMonthOrder.put("December", 12);
 				try {
 					String name1 = o1.getName();
 					String name2 = o2.getName();
 					if(name1.contains(AppConstants.ENTRY) && name2.contains(AppConstants.ENTRY)) {
+						//sort entries by date
 						Integer date1 = Integer.parseInt(name1.substring((name1.indexOf("-") + 1), (name1.indexOf("."))));
 						Integer date2 = Integer.parseInt(name2.substring((name2.indexOf("-") + 1), (name2.indexOf("."))));
 						return date1.compareTo(date2);
+					}else if(correctMonthOrder.get(name1) != null && correctMonthOrder.get(name2) != null) {
+						return correctMonthOrder.get(name1).compareTo(correctMonthOrder.get(name2));
+						//sort months.
 					}else {
+						//this should handle the sorting by years, and anything else we missed. 
 						return o1.compareTo(o2);			
 					}
 				}catch(Exception e) {
